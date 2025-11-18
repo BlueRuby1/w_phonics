e7274e28f0da081c491f24fa82ce5b34693811c9
Ayomiposi <ayomiposialadesuyi@gmail.com>
2025-11-18 11:13:47 +0100

    I Started a new app

---
+++ android/app/src/main/kotlin/com/example/yoru_phonics/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yoru_phonics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:yoru_phonics/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'W Phonics',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
+        primaryColor: Colors.amberAccent,
+        textTheme: TextTheme(
+          headlineMedium: TextStyle(color: Colors.white),
+          headlineSmall: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          bodySmall: TextStyle(color: Colors.white),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(8)),
+            fixedSize: Size.fromHeight(50),
+            textStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            )
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
